9a1f35b112bfca811ed9492bba637c95c97edb53
Hari <gh558383@gmail.com>
2025-07-17 13:53:33 +0530

    Completed Zudu AI backend assignment - Agent + LiveKit integration

--- agent-module/src/main/java/com/example/agent_module/controller/LiveKitController.java
+++ agent-module/src/main/java/com/example/agent_module/controller/LiveKitController.java
@@ -1,26 +1,26 @@
-//package com.example.agent_module.controller;
-//
-//import com.example.agent_module.service.LiveKitTokenService;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/livekit")
-//public class LiveKitController {
-//
-//    private final LiveKitTokenService tokenService;
-//
-//    public LiveKitController(LiveKitTokenService tokenService) {
-//        this.tokenService = tokenService;
-//    }
-//
-//    @GetMapping("/token")
-//    public String getToken(
-//            @RequestParam String identity,
-//            @RequestParam String room
-//    ) {
-//        return tokenService.generateToken(identity, room);
-//    }
-//}
+package com.example.agent_module.controller;
+
+import com.example.agent_module.service.LiveKitTokenService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/livekit")
+public class LiveKitController {
+
+    private final LiveKitTokenService tokenService;
+
+    public LiveKitController(LiveKitTokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @GetMapping("/token")
+    public String getToken(
+            @RequestParam String identity,
+            @RequestParam String room
+    ) {
+        return tokenService.generateToken(identity, room);
+    }
+}
 
 
 
